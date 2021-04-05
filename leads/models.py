5c39235fe48c4cdb45c5834ee20fd7d90e2ce800
Alexander Lucano <sailex1030@gmail.com>
2021-04-04 22:56:48 -0500

    Configurando inicio de proyecto , models , app

---
+++ leads/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+# User = get_user_model()
+
+
+class User(AbstractUser):
+    pass
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    # Foreing key
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    # OneToOneField tgiene que ir relacionado a un foreingkey
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # estos 2 lo tenemos dentro de abstractUser
+    # first_name = models.CharField(max_length=20)
+    # last_name = models.CharField(max_length=20)
