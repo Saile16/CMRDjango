f6310672908cbf2be7dc0d2d48bb64b5bcf7adf8
Alexander Lucano <sailex1030@gmail.com>
2021-04-05 21:04:37 -0500

    Agregamos estilos tailwind,modificamos algunas views , trataremos de cambiar algunas viewfuncion en clases

--- leads/views.py
+++ leads/views.py
@@ -5,6 +5,10 @@ from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
 
+def landing_page(request):
+    return render(request, "landing.html")
+
+
 def lead_list(request):
     # return HttpResponse("Hello World")
     # return render(request, "leads/home_page.html")
@@ -37,6 +41,52 @@ def lead_create(request):
         "form": form,
     }
     return render(request, "leads/lead_create.html", context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        # print("Receiving a post request")
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form,
+        "lead": lead
+    }
+
+    return render(request, "leads/lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm()
+#     if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             print("form is valid")
+#             print(form.cleaned_data)
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             agent = Agent.objects.first()
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save()
+#             print("The lead has been created")
+#             return redirect("/leads")
+#     context = {
+#         "form": form,
+#         "lead": lead
+#     }
+#     return render(request, "leads/lead_update.html", context)
 
 # def lead_create(request):
 #     form = LeadForm()
